6415887304fa1403a795bebdb4cc078bf43a7f9c
Bristovic <103121649+Bristovic@users.noreply.github.com>
2024-02-01 12:49:31 +0100

    Some brief updates to 1_3 and all 1_4
    The brief pygame example doesn't work from PyCharm and opens but freezes from python.exe (both 3.11 and 3.12).

--- PEII/Module1_ModulePackagesPIP/Module1_3_ModulesandPackages/second_package_use.py
+++ PEII/Module1_ModulePackagesPIP/Module1_3_ModulesandPackages/second_package_use.py
@@ -1,17 +1,27 @@
 from sys import path
 
-path.append("ex_package\\packages\\")
+# path.append("ex_package\\packages\\")
 
 # getting to bottom layers of file tree
-import extra.good.best.sigma
-from extra.good.best.tau import FunT
+#import extra.good.best.sigma
+#from extra.good.best.tau import FunT
 
-print(extra.good.best.sigma.FunS())
-print(FunT())
+##print(extra.good.best.sigma.FunS())
+#print(FunT())
 
 # using aliasing to make things easier
-import extra.good.best.sigma as sig
-import extra.good.alpha as alp
+#import extra.good.best.sigma as sig
+#import extra.good.alpha as alp
 
-print(sig.FunS())
-print(alp.FunA())
+#print(sig.FunS())
+#print(alp.FunA())
+
+# using a zip
+# path.append("ex_package\\ex.zip")
+
+#import ex.good.best.sigma as siggy
+
+#print(siggy.FunS())
+
+# can also do this instead of changing path (bit clunky maybe)
+import ex_package.packages.extra.good.alpha
